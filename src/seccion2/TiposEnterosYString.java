e1040d36b0bc9e281605a0c17d4a345ad5f439aa
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-11-25 14:28:23 -0500

    Sección 2.2: Variables tipo entero y tipo String

---
+++ src/seccion2/TiposEnterosYString.java
@@ -0,0 +1,20 @@
+package seccion2;
+
+public class TiposEnterosYString {
+    public static void main(String[] args) {
+
+        //Definimos una variable de tipo entero
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        //Definimos una variable de tipo 'String'
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Hola";
+        System.out.println(miVariableCadena);
+    }
+}
