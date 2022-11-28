4d23dbdbfa851c92820c9eac5140d83542848a6c
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-11-28 07:18:15 -0500

    Sección 2.4: Reglas para definir el nombre de una variable en Java

---
+++ src/seccion2/ReglasParaDefinirVariable.java
@@ -0,0 +1,16 @@
+package seccion2;
+
+public class ReglasParaDefinirVariable {
+
+    public static void main(String[] args) {
+        //Primer letra de la primera palabra en minúscula
+        //Si lleva varias palabras, la primer letra en mayúscula
+        //No puede comenzar con números ni caracteres especiales
+        //Puede empezar con guión bajo o signo pesos | _ | $ |
+        //No se recomienda utilizar tildes
+
+        int miVariable = 1;
+        int _miVariable = 2;
+        int $miVariable = 3;
+    }
+}
