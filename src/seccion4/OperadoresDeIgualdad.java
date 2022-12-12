e3954a5741a8a8d685e74154a34199e4eaa7ca65
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 07:06:16 -0500

    Sección 4.5: Operadores Relacionales en Java

--- src/seccion4/OperadoresDeIgualdad.java
+++ src/seccion4/OperadoresDeIgualdad.java
@@ -1,6 +1,6 @@
 package seccion4;
 
-public class OperadoresIgualdadYRelacionales {
+public class OperadoresDeIgualdad {
     public static void main(String[] args) {
         int a = 3;
         int b = 2;
---
+++ src/seccion4/OperadoresRelacionales.java
@@ -0,0 +1,26 @@
+package seccion4;
+
+public class OperadoresRelacionales {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        //Mayor o igual que
+        boolean c = (a >= b);
+        System.out.println(c);
+
+        //Mayor que
+        boolean d = a > b;
+        System.out.println(d);
+
+        //Menor que
+        boolean e = a < b;
+        System.out.println(e);
+
+        //Menor o igual que
+        boolean f = a <= b;
+        System.out.println(f);
+
+
+    }
+}
