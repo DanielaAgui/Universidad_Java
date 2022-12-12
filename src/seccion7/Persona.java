3c7b8eefd8ca40f99e59d740231565228c2620a8
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 12:47:40 -0500

    Sección 7.2: Atributos y métodos de una clase

--- src/seccion7/Persona.java
+++ src/seccion7/Persona.java
@@ -1,8 +1,16 @@
 package seccion7;
 
+//Una clase es una plantilla con atributos y metodos de la cual podemos crear objetos (instancias)
 //Nombre de la clase debe iniciar con mayuscula
 public class Persona {
-    public static void main(String[] args) {
-        //Una clase es una plantilla con atributos y metodos de la cual podemos crear objetos (instancias)
+    //Los atributos se deben declarar al inicio de la clase
+    String nombre;
+    String apellido;
+
+    //Metodos de la clase
+    //Parte de codigo que se puede reutilizar
+    public void desplegarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
     }
 }
