5395894975ee09ba131573516d5d90f0a7dd2bcb
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 09:35:54 -0500

    Sección 6.3: Ciclo 'for'

---
+++ src/seccion6/CicloFor.java
@@ -0,0 +1,10 @@
+package seccion6;
+
+public class CicloFor {
+    public static void main(String[] args) {
+
+        for (int contador = 0; contador < 3; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
