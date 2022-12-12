843c1105acc747377f173382c3749f8a296e3fc1
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 13:23:28 -0500

    Sección 8.1-2: Creación de métodos

---
+++ src/seccion8/Aritmetica.java
@@ -0,0 +1,15 @@
+package seccion8;
+
+public class Aritmetica {
+    int a;
+    int b;
+
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println(resultado);
+    }
+
+    public int sumarConRetorno() {
+        return a + b;
+    }
+}
---
+++ src/seccion8/PruebaAritmetica.java
@@ -0,0 +1,15 @@
+package seccion8;
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        Aritmetica aritmetica = new Aritmetica();
+
+        aritmetica.a = 3;
+        aritmetica.b = 2;
+        aritmetica.sumar();
+
+        //Para llamar un metodo con retorno, debemos crear una variable
+        int resultado = aritmetica.sumarConRetorno();
+        System.out.println(resultado);
+    }
+}
