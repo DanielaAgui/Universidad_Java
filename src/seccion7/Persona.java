43582bd32bef37a191bf28235cbc1b3afde34bad
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 12:40:24 -0500

    Sección 7.1: Clases en Java

---
+++ src/seccion7/Persona.java
@@ -0,0 +1,8 @@
+package seccion7;
+
+//Nombre de la clase debe iniciar con mayuscula
+public class Persona {
+    public static void main(String[] args) {
+        //Una clase es una plantilla con atributos y metodos de la cual podemos crear objetos (instancias)
+    }
+}
