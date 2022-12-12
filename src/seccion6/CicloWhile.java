e5e0ee296afdf0f80dfda0e667a02c4133a83e7f
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-12 09:24:17 -0500

    Sección 6.1: Ciclo 'while'

---
+++ src/seccion6/CicloWhile.java
@@ -0,0 +1,13 @@
+package seccion6;
+
+public class CicloWhile {
+    public static void main(String[] args) {
+        //Se ejecuta una sentencia mientras la condición sea verdadera
+        int contador = 0;
+        
+        while (contador < 3) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
